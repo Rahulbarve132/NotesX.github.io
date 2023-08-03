92ce4f8e79d4ca9a582f2a3850f7b8e1805fd9ac
Rahulbarve132 <99760468+Rahulbarve132@users.noreply.github.com>
2023-08-03 17:36:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,85 @@
+const addBtn = document.querySelector("#addBtn");
+const main = document.querySelector("#main");
+
+const saveNotes = () => {
+    const notes = document.querySelectorAll(".note textarea");
+    console.log(notes);
+    const data = [];
+    notes.forEach(
+        (note) => {
+            data.push(note.value)
+        }
+    )
+    
+        if (data.length === 0 ){
+            localStorage.removeItem("notes")
+        }
+        else{
+            localStorage.setItem("notes", JSON.stringify(data))
+        }
+   
+}
+
+addBtn.addEventListener(
+    "click",
+    function () {
+        addNote()
+    }
+) 
+
+    
+
+const addNote = (text = "") => {
+    const note = document.createElement("div");
+    note.classList.add("note")
+    note.innerHTML = ` 
+    <div class="Notes">
+    <div class="tool">
+        <i class="save fa-solid fa-floppy-disk"></i>
+        <i class="trash fa-solid fa-trash"></i>
+    </div>
+    
+    <textarea>${text}</textarea>
+    </div>
+`
+    note.querySelector(".trash").addEventListener(
+        "click",
+        function () {
+            note.remove()
+            saveNotes()
+        }
+    )
+
+    note.querySelector("textarea").addEventListener(
+        "focusout",
+        function(){
+            saveNotes()
+        }
+    )
+
+    note.querySelector(".save").addEventListener(
+        "click",
+        function () {
+            saveNotes()
+        }
+
+    )
+    main.appendChild(note);
+    saveNotes();
+}
+
+
+(
+    function () {
+        const lsnotes = JSON.parse(localStorage.getItem("notes"));
+        if(lsnotes === null){
+            addNote()
+        }else{
+            lsnotes.forEach(
+                (lsnotes) =>{
+                    addNote(lsnotes)
+                }   
+            )   
+        }       
+    }
+)()
